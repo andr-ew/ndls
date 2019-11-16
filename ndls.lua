0848d9ac3bdd2a9312f7f2f5bf0a747b85cdab47
AndrewShike <andrew.shike@regalcuttingtools.com>
2019-11-15 19:33:36 -0600

    trying to screen

--- ndls.lua
+++ ndls.lua
@@ -1,14 +1,19 @@
-n = nest:new()
+include "nest_"
+
+g_ = grid_:new()
+s_ = screen_:new()
+
+g_.channels = {}
 
 for i = 1, 4 do
-    n[i] = {
+    g_.channels[i] = {
         top = {
             gene = gene:new{ y = i },
             divide = momentary:new{
                 x = 0,
                 output.disabled = true
             },
-            speed = glide:new{
+            speed = Glide:new{
                 x = {1, 14},
                 v= 7
             },
@@ -52,13 +57,28 @@ for i = 1, 4 do
             toggle2 = toggle:new{
                 x = 13
             },
-            toggle3 = toggle:new{
-                x = 14
-            },
             pattern = pattern:new{
-                x = 15,
+                x = 14,
                 target = n
             }
         }
     }
 end
+
+s_.sect = {  ---?
+    
+}
+
+g_.pager = value:new{
+    x = 15,
+    y = {4, 7},
+    bg = 4
+}
+
+s_.pager = tabs:new{
+        x = { LEFT, RIGHT }, --no
+        count = 3
+    }
+} 
+
+
