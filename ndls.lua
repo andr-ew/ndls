f7e24d994cbf4e26646ecb38e8d8ce03011ccfe5
AndrewShike <andrew.shike@regalcuttingtools.com>
2019-11-16 22:42:20 -0600

    full sketch

--- ndls.lua
+++ ndls.lua
@@ -1,84 +1,163 @@
-include "nest_"
+include "nest_000000"
 
-g_ = grid_:new()
-s_ = screen_:new()
+local g_ = grid_:new()
+local s_ = screen_:new()
+local sc_ = softcut_:new(1, 2, 3, 4)
 
 g_.channels = {}
+s_.channels = {}
 
 for i = 1, 4 do
     g_.channels[i] = {
-        top = {
-            gene = gene:new{ y = i },
-            divide = momentary:new{
-                x = 0,
-                output.disabled = true
-            },
-            speed = Glide:new{
-                x = {1, 14},
-                v= 7
-            },
-            phase = value:new{
-                x = {0, 15},
-                input.disabled = true,
-                output.z = -1
-            },
-            reverse = toggle:new{
-                x = 15,
-                output.disabled = true
+        looper = softlooper_:new(sc_, i),
+        stutter = grid_momentary:new{
+            x = 0,
+            y = i,
+            output.enabled = false,
+            link = g_.channels[i].looper.stutter
+        },
+        speed = grid_glide:new{
+            x = {1, 14},
+            y = i,
+            v = 7,
+            link = g_.channels[i].looper.rateglide
+        },
+        phase = grid_value:new{
+            x = {0, 15},
+            y = i,
+            input.enabled = false,
+            output.z = -1,
+            link = sc_[i].phase
+        },
+        reverse = grid_toggle:new{
+            x = 15,
+            y = i,
+            output.enabled = false,
+            link = g_.channels[i].looper.reverse
+        },
+        record = grid_toggle:new{
+            x = 0,
+            y = i + 4,
+            off = 4,
+            link = g_.channels[i].looper.partyrecord
+        },
+        play = grid_toggle:new{
+            x = 1,
+            y = i + 4,
+            link = g_.channels[i].looper.partyplay
+        },
+        buffer = value:new{
+            x = {2, 5},
+            y = i + 4,
+            v = i,
+            link = g_.channels[i].looper.segment
+        },
+        sync = grid_toggle:new{
+            x = 6,
+            y = i + 4,
+            off = 4,
+            link = g_.channels[i].looper.sync
+        },
+        route = grid_toggle:new{
+            x = {7, 10},
+            y = i + 4,
+            event = function(v) {
+                for i = 1, 4 do
+                    sc_[i].levelcutcut = { i, v }
+                end
             }
         },
-        bottom = {
-            gene = gene:new{ y = i + 4 },
-            record = toggle:new{
-                x = 0,
-                off = 4
-            },
-            play = toggle:new{
-                x = 1
-            },
-            buffer = value:new{
-                x = {2, 5},
-                v = i
-            },
-            sync = toggle:new{
-                x = 6,
-                off = 4
-            },
-            route = momentary:new{
-                x = {7, 10}
-            },
-            glide = toggle:new{
-                x = 11,
-                off = 4
-            },
-            toggle1 = toggle:new{
-                x = 12
-            },
-            toggle2 = toggle:new{
-                x = 13
-            },
-            pattern = pattern:new{
-                x = 14,
-                target = n
+        glideon = grid_toggle:new{
+            x = 11,
+            y = i + 4,
+            off = 4,
+            event = function() {
+                
             }
+        },
+        toggle1 = grid_toggle:new{
+            x = 12,
+            y = i + 4
+        },
+        toggle2 = grid_toggle:new{
+            x = 13,
+            y = i + 4
+        },
+        pattern = grid_pattern:new{
+            x = 14,
+            y = i + 4,
+            target = g_
+        }
+    }
+    
+    s_.channels[i] = {
+        level = screen_value:new{
+            enabled = function() return s_.pager.set(i) end,
+            yalign = CENTER,
+            x = 1,
+            y = 1,
+            input = _enc_value:new{
+                n = 2,
+                range = { 0, 1.2 }
+            },
+            label = "level"
+        },
+        rate = screen_value:new{
+            enabled = function() return s_.pager.set(i) end,
+            yalign = CENTER,
+            x = 2,
+            y = 1,
+            input = _enc_value:new{
+                n = 3,
+                range = { 0.5, 2 }
+            },
+            label = "rate",
+            link = g_.channels[i].looper.rate2
         }
     }
 end
 
-s_.sect = {  ---?
-    
-}
-
 g_.pager = value:new{
     x = 15,
     y = {4, 7},
-    bg = 4
+    bg = 4,
+    link = s_.pager
 }
 
-s_.pager = tabs:new{
-        x = { LEFT, RIGHT }, --no
-        count = 3
-    }
-} 
+s_._.sections = {
+    x = { MAX, MAX },
+    y = { MAX, MAX, MIN }
+}
 
+s_.pager = screen_tabs:new{
+    x = {1, 2},
+    y = 3,
+    count = 4,
+    v = 0
+}
 
+s_.prev = screen_momentary:new{
+    x = 1,
+    y = 2,
+    yalign = CENTER,
+    input = _key_momentary:new{
+        n = 2
+    },
+    label = "prev",
+    event = function()
+        g_.pager.set(wrap(g_.pager.get() - 1, 0, 4))
+    end
+}
+
+s_.nxt = screen_momentary:new{
+    x = 2,
+    y = 2,
+    yalign = CENTER,
+    input = _key_momentary:new{
+        n = 3
+    },
+    label = "next",
+    event = function()
+        g_.pager.set(wrap(g_.pager.get() + 1, 0, 4))
+    end
+}
