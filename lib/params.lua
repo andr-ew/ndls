b5b2426a82969f1ad9768b4d585bb7dd0af84cd9
andr-ew <andrewcshike@gmail.com>
2022-11-04 21:11:21 -0500

    refactor params.lua

--- lib/params.lua
+++ lib/params.lua
@@ -1,8 +1,117 @@
+-- add normal params
+do
+    params:add_separator('params')
+
+    for i = 1, voices do 
+        params:add_group('track '..i, 6 + buffers)
+
+        params:add{
+            name = 'rec', id = 'rec '..i,
+            type = 'binary', behavior = 'toggle', 
+            action = function(v)
+                local n = i
+
+                sc.oldmx[n].rec = v; sc.oldmx:update(n)
+
+                local z = sc.buffer[n]
+                if not sc.punch_in[z].recorded then
+                    sc.punch_in:set(z, v)
+
+                    --TODO: refactor reset call into sc.punch_in
+                    if v==0 and sc.punch_in[z].recorded then 
+                        preset:reset(n)
+                    end
+                end
+
+                nest.grid.make_dirty()
+                nest.screen.make_dirty()
+            end
+        }
+        params:add{
+            name = 'reset', id = 'clear '..i,
+            type = 'binary', behavior = 'trigger', 
+            action = function()
+                local n = i
+                local b = sc.buffer[n]
+
+                params:set('rec '..i, 0) 
+                sc.punch_in:clear(b)
+
+                for ii = 1, voices do
+                    mparams:reset(ii, b, 'base')
+                    --wparams:reset(ii, b, 'base')
+                end
+                mparams:bang(n)
+                --wparams:bang(n)
+
+                nest.grid.make_dirty()
+                nest.screen.make_dirty()
+            end
+        }
+
+        params:add{
+            name = 'bend', id = 'bnd '..i,
+            type = 'control', controlspec = cs.def{ min = -1, max = 1, default = 0 },
+            action = function(v) 
+                sc.ratemx[i].bnd = v; sc.ratemx:update(i) 
+                nest.screen.make_dirty(); nest.arc.make_dirty()
+            end
+        }
+        params:add{
+            name = 'buffer', id = 'buffer '..i,
+            type = 'number', min = 1, max = buffers, default = i,
+            action = function(v)
+                sc.buffer[i] = v; sc.buffer:update(i)
+
+                nest.arc.make_dirty()
+                nest.screen.make_dirty()
+                nest.grid.make_dirty()
+            end
+        }
+        for b = 1,buffers do
+            params:add{
+                name = 'buffer '..b..' preset', id = 'preset '..i..' buffer '..b,
+                type = 'number', min = 1, max = presets, default = 1,
+                action = function(v)
+                    preset[i][b] = v; preset:update(i, b)
+
+                    nest.arc.make_dirty()
+                    nest.screen.make_dirty()
+                    nest.grid.make_dirty()
+                end
+            }
+        end
+        params:add{
+            name = 'send', id = 'send '..i,
+            type = 'binary', behavior = 'toggle', default = 1,
+            action = function(v) 
+                sc.sendmx[i].send = v; sc.sendmx:update() 
+
+                if v > 0 and params:get('return '..i) > 0 then
+                    sc.sendmx[i].ret = 0; sc.sendmx:update() 
+                    params:set('return '..i, 0, true)
+                end
+                nest.grid.make_dirty()
+            end
+        }
+        params:add{
+            name = 'return', id = 'return '..i,
+            type = 'binary', behavior = 'toggle',
+            action = function(v) 
+                sc.sendmx[i].ret = v; sc.sendmx:update()
+
+                if v > 0 and params:get('send '..i) > 0 then
+                    sc.sendmx[i].send = 0; sc.sendmx:update() 
+                    params:set('send '..i, 0, true)
+                end
+                nest.grid.make_dirty()
+            end
+        }
+    end
+end
+
 -- add metaparams
 do
-    local mult = function(self, a, b, c)
-        return a * b
-    end
     --TODO: switch to decibels / exp
     mparams:add{
         id = 'vol',
@@ -114,121 +223,17 @@ do
 
     --TODO: send/return as single metaparam (option type)
     --TODO: rec overdub flag
-end
+    
+    params:add_separator('metaparams')
 
---------- TODO new menu layout -------------
+    params:add_group('global', mparams:global_params_count())
+    mparams:add_global_params()
 
--- add mappable params
-params:add_separator('base values')
-for i = 1, voices do 
-    params:add_group('track '..i, 6 + buffers + mparams:mappable_params_count(i))
+    params:add_group('track', mparams:track_params_count())
+    mparams:add_track_params()
 
-    params:add{
-        name = 'rec', id = 'rec '..i,
-        type = 'binary', behavior = 'toggle', 
-        action = function(v)
-            local n = i
-
-            sc.oldmx[n].rec = v; sc.oldmx:update(n)
-
-            local z = sc.buffer[n]
-            if not sc.punch_in[z].recorded then
-                sc.punch_in:set(z, v)
-
-                --TODO: refactor reset call into sc.punch_in
-                if v==0 and sc.punch_in[z].recorded then 
-                    preset:reset(n)
-                end
-            end
-
-            nest.grid.make_dirty()
-            nest.screen.make_dirty()
-        end
-    }
-    params:add{
-        name = 'reset', id = 'clear '..i,
-        type = 'binary', behavior = 'trigger', 
-        action = function()
-            local n = i
-            local b = sc.buffer[n]
-
-            params:set('rec '..i, 0) 
-            sc.punch_in:clear(b)
-
-            for ii = 1, voices do
-                mparams:reset(ii, b, 'base')
-                --wparams:reset(ii, b, 'base')
-            end
-            mparams:bang(n)
-            --wparams:bang(n)
-
-            nest.grid.make_dirty()
-            nest.screen.make_dirty()
-        end
-    }
-
-    local ids = mparams:add_mappable_params(i)
-
-    -- TODO: wparam mappables
-
-    params:add{
-        name = 'bend', id = 'bnd '..i,
-        type = 'control', controlspec = cs.def{ min = -1, max = 1, default = 0 },
-        action = function(v) 
-            sc.ratemx[i].bnd = v; sc.ratemx:update(i) 
-            nest.screen.make_dirty(); nest.arc.make_dirty()
-        end
-    }
-    params:add{
-        name = 'buffer', id = 'buffer '..i,
-        type = 'number', min = 1, max = buffers, default = i,
-        action = function(v)
-            sc.buffer[i] = v; sc.buffer:update(i)
-
-            nest.arc.make_dirty()
-            nest.screen.make_dirty()
-            nest.grid.make_dirty()
-        end
-    }
-    for b = 1,buffers do
-        params:add{
-            name = 'buffer '..b..' preset', id = 'preset '..i..' buffer '..b,
-            type = 'number', min = 1, max = presets, default = 1,
-            action = function(v)
-                preset[i][b] = v; preset:update(i, b)
-
-                nest.arc.make_dirty()
-                nest.screen.make_dirty()
-                nest.grid.make_dirty()
-            end
-        }
-    end
-    params:add{
-        name = 'send', id = 'send '..i,
-        type = 'binary', behavior = 'toggle', default = 1,
-        action = function(v) 
-            sc.sendmx[i].send = v; sc.sendmx:update() 
-
-            if v > 0 and params:get('return '..i) > 0 then
-                sc.sendmx[i].ret = 0; sc.sendmx:update() 
-                params:set('return '..i, 0, true)
-            end
-            nest.grid.make_dirty()
-        end
-    }
-    params:add{
-        name = 'return', id = 'return '..i,
-        type = 'binary', behavior = 'toggle',
-        action = function(v) 
-            sc.sendmx[i].ret = v; sc.sendmx:update()
-
-            if v > 0 and params:get('send '..i) > 0 then
-                sc.sendmx[i].send = 0; sc.sendmx:update() 
-                params:set('send '..i, 0, true)
-            end
-            nest.grid.make_dirty()
-        end
-    }
+    params:add_group('preset', mparams:preset_params_count())
+    mparams:add_preset_params()
 end
 
 -- add metaparam options
@@ -236,191 +241,8 @@ do
     params:add_separator('metaparam options')
 
     do
-        params:add_group('default view',  #mparams.list)
-
-        view_options.options = { 'preset', 'base' }
-        view_options.vals = { preset = 1, base = 2 }
-        local prst, base = view_options.vals.preset, view_options.vals.base
-        local defaults = { old = base, q = base }
-
-        for _,m in ipairs(mparams.list) do
-            local id = m.id
-            params:add{
-                name = id, id = id..'_view', type = 'option',
-                options = view_options.options, default = defaults[id] or prst,
-                allow_pmap = false,
-            }
-        end
-    end
-
-    do
-        params:add_group('reset actions', 2 + 9*3)
-    
-        do
-            params:add_separator('window')
-            do
-                local names = { 'random value', 'default value' }
-                local funcs = { windowparams.resets.random, windowparams.resets.default }
-                params:add{
-                    id = 'window_reset_presets', name = 'presets', type = 'option',
-                    options = names, default = 1, allow_pmap = false,
-                    action = function(v)
-                        wparams:set_reset('preset', funcs[v])
-                    end
-                }
-            end
-            --TODO: window base reset
-        end
-
-        local function add_reset_param(id, scope, names, funcs)
-            scope = scope or 'preset'
-            local sname = (scope == 'preset') and 'presets' or 'base'
-            params:add{
-                id = id..'_reset_'..sname, name = sname, type = 'option',
-                options = names, default = 1, allow_pmap = false,
-                action = function(v)
-                    mparams:set_reset(id, scope, funcs[v])
-                end
-            }
-        end
-
-        do
-            local id = 'old'
-            params:add_separator(id)
-            add_reset_param(
-                id, 'preset', 
-                { 'default value', 'random value', 'none' },
-                { metaparams.resets.default, metaparams.resets.random, metaparams.resets.none }
-            )
-            add_reset_param(
-                id, 'base',
-                { 'default value', 'none',  },
-                { metaparams.resets.default, metaparams.resets.none }
-            )
-        end
-        do
-            local id = 'vol'
-            params:add_separator(id)
-            add_reset_param(
-                id, 'preset', 
-                { 'default value', 'random value', 'none' },
-                { metaparams.resets.default, metaparams.resets.random, metaparams.resets.none }
-            )
-            
-            local function low_all(self, param_id)
-                local silent = true
-                params:set(param_id, 0, silent)
-            end
-            local function low_other(self, param_id, t, b)
-                local silent = true
-                local v = (t==b) and 1 or 0
-                params:set(param_id, v, silent)
-            end
-
-            add_reset_param(
-                id, 'base',
-                { 'default value', 'low (all buffers)', 'low (other buffers)', 'none' },
-                {  windowparams.resets.default, low_all, low_other, windowparams.resets.none }
-            )
-        end
-        do
-            local id = 'pan'
-            params:add_separator(id)
-            add_reset_param(
-                id, 'preset', 
-                { 'default value', 'random value', 'none' },
-                { metaparams.resets.default, metaparams.resets.random, metaparams.resets.none }
-            )
-            add_reset_param(
-                id, 'base',
-                { 'default value', 'none',  },
-                { metaparams.resets.default, metaparams.resets.none }
-            )
-        end
-        do
-            local id = 'q'
-            params:add_separator(id)
-            add_reset_param(
-                id, 'preset', 
-                { 'default value', 'random value', 'none' },
-                { metaparams.resets.default, metaparams.resets.random, metaparams.resets.none }
-            )
-            add_reset_param(
-                id, 'base',
-                { 'default value', 'none',  },
-                { metaparams.resets.default, metaparams.resets.none }
-            )
-        end
-        do
-            local id = 'cut'
-            params:add_separator(id)
-            add_reset_param(
-                id, 'preset', 
-                { 'random value', 'default value', 'none' },
-                { metaparams.resets.random, metaparams.resets.default, metaparams.resets.none }
-            )
-            add_reset_param(
-                id, 'base',
-                { 'default value', 'none' },
-                { metaparams.resets.default, metaparams.resets.none }
-            )
-        end
-        do
-            local id = 'type'
-            params:add_separator(id)
-            add_reset_param(
-                id, 'preset', 
-                { 'default value', 'random value', 'none' },
-                { metaparams.resets.default, metaparams.resets.random, metaparams.resets.none }
-            )
-            add_reset_param(
-                id, 'base',
-                { 'default value', 'none' },
-                { metaparams.resets.default, metaparams.resets.none }
-            )
-        end
-        do
-            local id = 'loop'
-            params:add_separator(id)
-            add_reset_param(
-                id, 'preset', 
-                { 'default value', 'random value', 'none' },
-                { metaparams.resets.default, metaparams.resets.random, metaparams.resets.none }
-            )
-            add_reset_param(
-                id, 'base',
-                { 'default value', 'none' },
-                { metaparams.resets.default, metaparams.resets.none }
-            )
-        end
-        do
-            local id = 'rate'
-            params:add_separator(id)
-            add_reset_param(
-                id, 'preset', 
-                { 'random value', 'default value', 'none' },
-                { metaparams.resets.random, metaparams.resets.default, metaparams.resets.none }
-            )
-            add_reset_param(
-                id, 'base',
-                { 'default value', 'none' },
-                { metaparams.resets.default, metaparams.resets.none }
-            )
-        end
-        do
-            local id = 'rev'
-            params:add_separator(id)
-            add_reset_param(
-                id, 'preset', 
-                { 'random value', 'default value', 'none' },
-                { metaparams.resets.random, metaparams.resets.default, metaparams.resets.none }
-            )
-            add_reset_param(
-                id, 'base',
-                { 'default value', 'none' },
-                { metaparams.resets.default, metaparams.resets.none }
-            )
-        end
+        params:add_group('scopes', mparams:scope_params_count())
+        mparams:add_scope_params()
     end
 
     do
@@ -442,20 +264,6 @@ do
     end
 
     --TODO: slew group
-
-    do
-        params:add_group('raw data', (
-            wparams:base_params_count()
-            + wparams:preset_params_count()
-            + mparams:base_params_count()
-            + mparams:preset_params_count()
-        ))
-
-        wparams:add_base_params()
-        wparams:add_preset_params()
-        mparams:add_base_params()
-        mparams:add_preset_params()
-    end
 end
 
 -- add softcut options
